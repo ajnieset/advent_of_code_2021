610112751561ab3657cf754a283b49691d30ec08
Austin Nieset <ajnieset@gmail.com>
2021-12-02 18:50:54 -0500

    completed part 2 of day 1 and renamed file

--- day1/solution.py
+++ day1/solution.py
@@ -19,13 +19,34 @@ def count_measurement_increases(depths) -> int:
     
     return increases
 
+def calculate_sums(depths):
+    set1 = set2 = set3 = None
+    sums_of_depths = []
+
+    for depth in depths:
+        depth = int(depth)
+        set3 = set2
+        set2 = set1
+        set1 = depth
+        if None in [set1, set2, set3]:
+            continue
+        
+        depth_sum = set1 + set2 + set3
+        sums_of_depths.append(depth_sum)
+    
+    return sums_of_depths
+        
+
+
 def main():
     with open("input.csv", 'r') as depths:
-        increases = count_measurement_increases(depths)
+        summed_depths = calculate_sums(depths)
+        
+    print(summed_depths)
+    increases = count_measurement_increases(summed_depths)
 
     print(f"Number of increases: {increases}")
     
-
 
 if __name__ == "__main__":
     main()
