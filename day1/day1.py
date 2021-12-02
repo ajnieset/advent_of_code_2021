e32679d958dc4913f7b348f8a8ffd2aef7d2a8f6
Austin Nieset <ajnieset@gmail.com>
2021-12-02 18:36:48 -0500

    Completed Part 1 of Day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,8 +1,31 @@
-def check_if_increase(prev: int, depth: int):
-    pass
+from typing import Literal
 
-def count_measurement_increases():
-    pass
+
+def check_if_increase(prev: int, depth: int) -> Literal[1, 0]:
+    return 1 if prev < depth else 0
+
+def count_measurement_increases(depths) -> int:
+    increases = 0
+    prev = None
+
+    for depth in depths:
+        depth = int(depth)
+        if prev is None:
+            prev = depth
+            continue
+        
+        increases += check_if_increase(prev, depth)
+        prev = depth
+    
+    return increases
+
+def main():
+    with open("input.csv", 'r') as depths:
+        increases = count_measurement_increases(depths)
+
+    print(f"Number of increases: {increases}")
+    
+
 
 if __name__ == "__main__":
-    count_measurement_increases()
+    main()
